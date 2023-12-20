6cf534b9005d32c245eb139fcb120529dfffe34b
zcbe <151560157+zcbe@users.noreply.github.com>
2023-12-20 15:25:06 +0100

    step 4 add function and images

--- assets/script.js
+++ assets/script.js
@@ -17,13 +17,50 @@ const slides = [
 	}
 ];
 
+// recherche des variables dans html
 const bannerImg = document.querySelector('.banner-img');
 const arrow_left = document.querySelector('.arrow_left');
 const arrow_right = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot'); 
+
+let currentIndex=0;
+
+// Fonction pour mettre à jour les points indicateurs
+function updateDots(index) {
+    dots.forEach((dot, i) => {
+        if (i === index) {                     //si le point est à stricetement égale à l'index
+            dot.classList.add('dot_selected'); // Ajoutez la classe pour le point actuel
+        } else {
+            dot.classList.remove('dot_selected'); // Supprimez la classe pour les autres points
+        }
+    });
+}
+
+// Fonction pour mettre l'image et le texte
+function updateCarousel(index, direction) {
+  
+  // Mettre à jour l'image
+  bannerImg.src = `assets/images/slideshow/${slides[currentIndex].image}`;
+  bannerImg.alt = `Slide ${currentIndex +1}`;
+
+  // Mettre à jour le texte
+  document.querySelector('p').innerHTML = slides[currentIndex].tagLine;
+  console.log(`Clic sur la flèche ${direction}`);
+}
+
+
 
 
 // Gestionnaire d'événement pour le clic sur la flèche gauche
-arrow_left.addEventListener('click', () => {console.log("tu as cliqué sur flèche de gauche")})
+arrow_left.addEventListener('click', function () {
+    currentIndex = (currentIndex - 1);
+    updateCarousel(currentIndex, 'left');
+    updateDots(currentIndex); // Mettez à jour les points indicateurs
+});
 
 // Gestionnaire d'événement pour le clic sur la flèche droite
-arrow_right.addEventListener('click', () => {console.log("tu as cliqué sur flèche de droite")})
+arrow_right.addEventListener('click', function () {
+    currentIndex = (currentIndex + 1) ;
+    updateCarousel(currentIndex, 'right');
+    updateDots(currentIndex); // Mettez à jour les points indicateurs
+});
