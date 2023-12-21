c09f004656de85c1e88ebb792fb64d5980ed0271
zcbe <151560157+zcbe@users.noreply.github.com>
2023-12-21 23:09:26 +0100

    refactor

--- assets/script.js
+++ assets/script.js
@@ -18,17 +18,30 @@ const slides = [
 ];
 
 // recherche des variables dans html
-const bannerImg = document.querySelector('.banner-img');
-const arrow_left = document.querySelector('.arrow_left');
-const arrow_right = document.querySelector('.arrow_right');
+const banner_Image = document.querySelector('.banner-image');
+const arrow_Left = document.querySelector('.arrow-left');
+const arrow_Right = document.querySelector('.arrow-right');
 const dots = document.querySelectorAll('.dot'); 
 
 let currentIndex=0;
 
+// Gestionnaire d'événement pour le clic sur la flèche gauche
+arrow_Left.addEventListener('click', function () {
+    currentIndex = (currentIndex - 1);
+    updateCarousel(currentIndex, 'Left');
+});
+
+// Gestionnaire d'événement pour le clic sur la flèche droite
+arrow_Right.addEventListener('click', function () {
+    currentIndex = (currentIndex + 1) ;
+    updateCarousel(currentIndex, 'Right');
+});
+
+
 // Fonction pour mettre à jour les points indicateurs
 function updateDots(index) {
     dots.forEach((dot, i) => {
-        if (i === index) {                     //si le point est à stricetement égale à l'index
+        if (i === index) {                     //si le point est à strictement égale à l'index
             dot.classList.add('dot_selected'); // Ajoutez la classe pour le point actuel
         } else {
             dot.classList.remove('dot_selected'); // Supprimez la classe pour les autres points
@@ -36,38 +49,24 @@ function updateDots(index) {
     });
 }
 
+
 // Fonction pour mettre l'image et le texte
-function updateCarousel(index, direction) {
+function updateCarousel(_index, direction) {
 	//correction du bug pour la première et la dernière image
-	if (currentIndex === -1 && direction === 'left') {
+	if (currentIndex === -1 && direction === 'Left') {
 		currentIndex = slides.length - 1; // length nombre de paramètres attendus  
-	} else if (currentIndex === slides.length && direction === 'right') {
+	} else if (currentIndex === slides.length && direction === 'Right') {
 		currentIndex = 0;
 	}
 
   
   // Mettre à jour l'image
-  bannerImg.src = `assets/images/slideshow/${slides[currentIndex].image}`;
-  bannerImg.alt = `Slide ${currentIndex +1}`;
+  banner_Image .src = `assets/images/slideshow/${slides[currentIndex].image}`;
+  banner_Image .alt = `Slide ${currentIndex +1}`;
 
   // Mettre à jour le texte
   document.querySelector('p').innerHTML = slides[currentIndex].tagLine;
   console.log(`Clic sur la flèche ${direction}`);
+  updateDots(currentIndex); // Mettez à jour les points indicateurs
+
 }
-
-
-
-
-// Gestionnaire d'événement pour le clic sur la flèche gauche
-arrow_left.addEventListener('click', function () {
-    currentIndex = (currentIndex - 1);
-    updateCarousel(currentIndex, 'left');
-    updateDots(currentIndex); // Mettez à jour les points indicateurs
-});
-
-// Gestionnaire d'événement pour le clic sur la flèche droite
-arrow_right.addEventListener('click', function () {
-    currentIndex = (currentIndex + 1) ;
-    updateCarousel(currentIndex, 'right');
-    updateDots(currentIndex); // Mettez à jour les points indicateurs
-});
